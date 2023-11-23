87b0b8bf580bfb77cae6c74aca3b0165c3ce1582
LucasCintra <106413229+LucasCintra10@users.noreply.github.com>
2023-11-23 09:10:09 -0300

    Chemical

--- App.js
+++ App.js
@@ -4,17 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.login}>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: '100%',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dce1eb',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  login: {
+    width: '90%',
+    height: 400,
+    backgroundColor: '#303646',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
   },
 });
